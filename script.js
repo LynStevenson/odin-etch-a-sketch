60a9c99dbe074b4386651510d1a84d27995437aa
LynStevenson <81272840+LynStevenson@users.noreply.github.com>
2024-05-23 20:56:45 -0500

    added darkening

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ function makeGrid(){
     const container = document.querySelector("#container");
     const sizePrompt = document.querySelector("#sizePrompt");
     function changeColor(e){
-        e.target.style.backgroundColor = ('#'+(0x1000000+Math.random()*0xffffff).toString(16).slice(1,7));
+        e.currentTarget.style.backgroundColor = ('#'+(0x1000000+Math.random()*0xffffff).toString(16).slice(1,7));
+    }
+    function darken(e){
+        e.currentTarget.style.opacity = Number(e.currentTarget.style.opacity) + 0.10;
     }
     function createBoxes(length){
         container.replaceChildren();
         document.documentElement.style.setProperty("--box-size", (400/length).toString() + "px");
         for (let i = 0; i < length*length; i++) {
             const newDiv = document.createElement("Div");
+            const darkeningDiv = document.createElement("Div");
+            darkeningDiv.style.backgroundColor = "black";
+            darkeningDiv.style.opacity = 0;
+            darkeningDiv.addEventListener("mouseover", darken);
             newDiv.addEventListener("mouseover", changeColor);
+            newDiv.appendChild(darkeningDiv);
             container.appendChild(newDiv);
         }
     }
